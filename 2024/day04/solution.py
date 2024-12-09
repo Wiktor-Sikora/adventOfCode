e3121601b55482da92ad294722deb667fc12b5ab
Wiktor-Sikora <sikora.wiktor777@gmail.com>
2024-12-09 16:15:41 +0100

    day 04

--- 2024/day04/solution.py
+++ 2024/day04/solution.py
@@ -40,44 +40,41 @@ class Solution:
 
     def _diagonal_search(self):
         answer = 0
-        pointer = 0
-        end = len(self.data) - 3
-        row_length_right = len(self.data[0]) - 3
-        row_length_left = len(self.data[0])
-        while pointer < end:
+        column_length_right = len(self.data[0]) - 3
+        column_length_left = len(self.data[0])
+        for row_index in range(len(self.data) - 3):
             # searching for \
-            for column_index in range(row_length_right):
-                if self.data[pointer][column_index] == 'X':
-                    if self.data[pointer + 1][column_index + 1] != 'M':
+            for column_index in range(column_length_right):
+                if self.data[row_index][column_index] == 'X':
+                    if self.data[row_index + 1][column_index + 1] != 'M':
                         continue
-                    if self.data[pointer + 2][column_index + 2] != 'A':
+                    if self.data[row_index + 2][column_index + 2] != 'A':
                         continue
-                    if self.data[pointer + 3][column_index + 3] == 'S':
+                    if self.data[row_index + 3][column_index + 3] == 'S':
                         answer += 1    
-                elif self.data[pointer][column_index] == 'S':
-                    if self.data[pointer + 1][column_index + 1] != 'A':
+                elif self.data[row_index][column_index] == 'S':
+                    if self.data[row_index + 1][column_index + 1] != 'A':
                         continue
-                    if self.data[pointer + 2][column_index + 2] != 'M':
+                    if self.data[row_index + 2][column_index + 2] != 'M':
                         continue
-                    if self.data[pointer + 3][column_index + 3] == 'X':
+                    if self.data[row_index + 3][column_index + 3] == 'X':
                         answer += 1
             # searching for /
-            for column_index in range(3, row_length_left):
-                if self.data[pointer][column_index] == 'X':
-                    if self.data[pointer + 1][column_index - 1] != 'M':
+            for column_index in range(3, column_length_left):
+                if self.data[row_index][column_index] == 'X':
+                    if self.data[row_index + 1][column_index - 1] != 'M':
                         continue
-                    if self.data[pointer + 2][column_index - 2] != 'A':
+                    if self.data[row_index + 2][column_index - 2] != 'A':
                         continue
-                    if self.data[pointer + 3][column_index - 3] == 'S':
+                    if self.data[row_index + 3][column_index - 3] == 'S':
                         answer += 1    
-                elif self.data[pointer][column_index] == 'S':
-                    if self.data[pointer + 1][column_index - 1] != 'A':
+                elif self.data[row_index][column_index] == 'S':
+                    if self.data[row_index + 1][column_index - 1] != 'A':
                         continue
-                    if self.data[pointer + 2][column_index - 2] != 'M':
+                    if self.data[row_index + 2][column_index - 2] != 'M':
                         continue
-                    if self.data[pointer + 3][column_index - 3] == 'X':
+                    if self.data[row_index + 3][column_index - 3] == 'X':
                         answer += 1
-            pointer += 1
         return answer
 
     def puzzle_one(self):
@@ -90,7 +87,20 @@ class Solution:
         return answer
 
     def puzzle_two(self):
-        pass
+        answer = 0
+        column_lenght = len(self.data) - 2
+        row_lenght = len(self.data[0]) - 2
+        for row_index in range(row_lenght):
+            for column_index in range(column_lenght):
+                if self.data[row_index + 1][column_index + 1] != 'A':
+                    continue
+                if ((self.data[row_index][column_index] == 'M' and self.data[row_index + 2][column_index + 2] == 'S') or\
+                   (self.data[row_index][column_index] == 'S' and self.data[row_index + 2][column_index + 2] == 'M')) and\
+                   ((self.data[row_index][column_index + 2] == 'M' and self.data[row_index + 2][column_index] == 'S') or\
+                   (self.data[row_index][column_index + 2] == 'S' and self.data[row_index + 2][column_index] == 'M')):
+                    answer += 1
+        return answer
+
 
 if __name__ == '__main__':
     solution = Solution()
