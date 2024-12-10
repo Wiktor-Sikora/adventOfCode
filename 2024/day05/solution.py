7fb1410fd879e9d14b195b05000792c0e1fb9e76
Wiktor-Sikora <wiktor.sikora777@gmail.com>
2024-12-10 19:47:25 +0100

    day 05 1/2

--- 2024/day05/solution.py
+++ 2024/day05/solution.py
@@ -13,25 +13,49 @@ class Solution:
 
         first_index = 0
         for rule_index in range(1, len(self.rules)):
-            print(counted_rules)
             if self.rules[rule_index][0] != self.rules[rule_index - 1][0]:
-                counted_rules[self.rules[rule_index - 1][0]] = (self.rules[rule_index - 1][0], first_index, rule_index - 1)
+                counted_rules[self.rules[rule_index - 1][0]] = (first_index, rule_index - 1)
                 first_index = rule_index
         return counted_rules
 
+    def _get_index(self, value:int, iterable:list):
+        for index in range(len(iterable) - 1):
+            if iterable[index] == value:
+                return index
+        else:
+            return False
+
+    def _is_line_valid(self, line:list):
+        for index, number in enumerate(line):
+            for rule in self.rules[self.counted_rules[number][0]:self.counted_rules[number][1]]:
+                second_number_index = self._get_index(rule[1], line)
+                if not second_number_index:
+                    continue
+                elif index > second_number_index:
+                    return False
+        return True
 
     def puzzle_one(self):
         self.get_data()
-        counted_rules = self._get_counted_rules()
-        print(counted_rules)
+        self.counted_rules = self._get_counted_rules()
 
-        for line in self.pages:
-            for page in line:
-                # print(self.rules[self._rules_binary_search(page)])
+        return sum([line[len(line) // 2] for line in self.pages if self._is_line_valid(line)])
 
+    def _correct_ordering(self, line):
+        pass
 
     def puzzle_two(self):
-        pass
+        self.get_data()
+        self.counted_rules = self._get_counted_rules
+        answer = 0
+
+        for line in self.pages:
+            if self._is_line_valid(line):
+                continue
+            
+            line = self._correct_ordering(line)
+            
+            answer += line[len(line) // 2]
 
 if __name__ == '__main__':
     solution = Solution()
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ I don't include input data files in the repo as per creators request
 
 ## My attempts:
 
-- [2024](/2024) - Python - 8/50 ⭐
+- [2024](/2024) - Python - 9/50 ⭐
